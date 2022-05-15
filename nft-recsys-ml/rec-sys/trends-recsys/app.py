ddef6726cb7f2c92066d3ce1ffeddac141bebf4b
dinuka-rp <drpiyadigama@gmail.com>
2022-05-16 00:56:32 +0530

    implement trends-recsys ml endpoint

--- nft-recsys-ml/rec-sys/trends-recsys/app.py
+++ nft-recsys-ml/rec-sys/trends-recsys/app.py
@@ -1,5 +1,6 @@
 # save this as app.py
-from flask import Flask
+from flask import Flask, jsonify, request
+from trends_content_nft_recsys import trends_based_recommendations
 
 app = Flask(__name__)
 
@@ -7,6 +8,19 @@ app = Flask(__name__)
 @app.route("/ping")
 def hello():
     return "pong"
+
+@app.route("/rec")
+def get_trait_similarity_recommendations():
+    if(request.method == 'GET'):
+        generated_recommendations = trends_based_recommendations()
+
+        data = {
+            'trends_featured_rec': {
+                'items': generated_recommendations,
+            }
+        }
+
+    return jsonify(data)
 
 # export FLASK_APP=app.py
 # python3 -m flask run
